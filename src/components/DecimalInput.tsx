21ec3167ee1b6cfcd691edf9dad78eb0867b61be
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2024-07-17 19:29:02 +0200

    feat: Add DecimalInput component for decimal input with custom buttons

---
+++ src/components/DecimalInput.tsx
@@ -0,0 +1,65 @@
+import { twMerge } from 'tailwind-merge';
+import FractionInputButtons from './FractionInputButtons';
+
+type Props = {
+  id: string;
+  name: string;
+  valueInMm: number;
+  setValueInMm: (val: number) => void;
+  className?: string;
+  step: number;
+  incrementValue: () => void;
+  decrementValue: () => void;
+};
+
+const DecimalInput = ({
+  id,
+  name,
+  valueInMm,
+  setValueInMm,
+  className,
+  step,
+  incrementValue,
+  decrementValue,
+}: Props) => {
+  return (
+    <div
+      className={twMerge(
+        'flex flex-row items-center justify-center mb-4 w-full',
+        className,
+      )}
+    >
+      {/* removing the number input arrows, since we have our own buttons */}
+      <style>
+        {`
+            input[type="number"]::-webkit-inner-spin-button,
+            input[type="number"]::-webkit-outer-spin-button {
+                -webkit-appearance: none;
+                margin: 0;
+            }
+
+            input[type="number"] {
+                -moz-appearance: textfield;
+            }
+        `}
+      </style>
+
+      <input
+        className='border border-gray-300 rounded-l py-2 px-3 text-gray-800'
+        id={id}
+        type='number'
+        step={step}
+        name={name}
+        value={valueInMm}
+        onChange={(e) => setValueInMm(Number(e.target.value))}
+      />
+
+      <FractionInputButtons
+        incrementValue={incrementValue}
+        decrementValue={decrementValue}
+      />
+    </div>
+  );
+};
+
+export default DecimalInput;
--- src/components/MmToInch.tsx
+++ src/components/MmToInch.tsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 import millimetersToInchesWithFractions from '../utils/millimetersToInchesWithFractions';
+import DecimalInput from './DecimalInput';
 
 const MmToInch = () => {
   const [valueInMm, setValueInMm] = useState(0);
+  const step = 0.01;
 
   const valueInInches = millimetersToInchesWithFractions(valueInMm);
+
+  const incrementValue = () => {
+    const calc = (valueInMm + step).toFixed(2);
+
+    setValueInMm(Number(calc));
+  };
+
+  const decrementValue = () => {
+    const calc = (valueInMm - step).toFixed(2);
+
+    setValueInMm(Number(calc));
+  };
 
   return (
     <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
@@ -15,13 +29,15 @@ const MmToInch = () => {
         >
           Enter mm to convert to inches
         </label>
-        <input
-          className='border border-gray-300 rounded py-2 px-3 text-gray-800'
+        <DecimalInput
           id='mm-to-inch'
           name='mm-to-inch'
-          value={valueInMm}
-            onChange={(e) => setValueInMm(Number(e.target.value))}
-
+          valueInMm={valueInMm}
+          setValueInMm={(val: number) => setValueInMm(val)}
+          step={step}
+          incrementValue={incrementValue}
+          decrementValue={decrementValue}
+          className='py-2 px-3 text-gray-800'
         />
       </div>
 
@@ -29,6 +45,7 @@ const MmToInch = () => {
         Result: <span id='result'>{valueInInches}</span> inches
       </p>
 
+      {/* TODO: Implement copy to clipboard function */}
       <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full mb-4'>
         Copy to clipboard
       </button>
